a9e250b4fc4150dc8bd6684e0796fd8430272c4b
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2025-12-18 09:42:23 +0900

    Add a function to transfer video frame to Direct2D bitmap.

--- AyarabuPlayer/mf_video_transferor.cpp
+++ AyarabuPlayer/mf_video_transferor.cpp
@@ -16,67 +16,64 @@ CMfVideoTransferor::~CMfVideoTransferor()
 	ReleaseWicBitmap();
 }
 /*転送*/
-bool CMfVideoTransferor::TransferVideoFrame(SVideoFrame* pVideoFrame)
+bool CMfVideoTransferor::TransferVideoFrame(SVideoFrame* pVideoFrame, long long* currentFrameTime)
 {
 	if (pVideoFrame == nullptr)return false;
 
-	if (m_pMfEngineEx != nullptr)
+	if (TransferVideoFrameToWicBitmap())
 	{
-		BOOL iRet = m_pMfEngineEx->HasVideo();
-		if (iRet)
+		if (currentFrameTime != nullptr)
 		{
-			long long llReadyFrame = 0;
-			HRESULT hr = m_pMfEngineEx->OnVideoStreamTick(&llReadyFrame);
-			if (SUCCEEDED(hr) && llReadyFrame >= 0)
-			{
-				pVideoFrame->llCurrentTime = GetCurrentTimeInMilliSeconds();
-
-				unsigned long ulDestWidth = 0;
-				unsigned long ulDestHeight = 0;
-				bool bRet = GetVideoSize(&ulDestWidth, &ulDestHeight);
-				if (!bRet)return false;
-
-				bRet = CheckWicBitmapSize(ulDestWidth, ulDestHeight);
-				if (!bRet)return false;
-
-				RECT dstRect{};
-				dstRect.right = ulDestWidth;
-				dstRect.bottom = ulDestHeight;
-				MFARGB bg{ 0, 0, 0, 0 };
-				MFVideoNormalizedRect normalisedRect{};
-				hr = m_pMfEngineEx->TransferVideoFrame(m_pWicBitmap, &normalisedRect, &dstRect, &bg);
-				if (FAILED(hr))return false;
-
-				unsigned int uiWidth = 0;
-				unsigned int uiHeight = 0;
-				hr = m_pWicBitmap->GetSize(&uiWidth, &uiHeight);
-				if (FAILED(hr))return false;
-
-				pVideoFrame->iWidth = uiWidth;
-				pVideoFrame->iHeight = uiHeight;
-
-				CComPtr<IWICBitmapLock> pWicBitmapLock;
-				WICRect wicRect{ 0, 0, pVideoFrame->iWidth, pVideoFrame->iHeight };
-				hr = m_pWicBitmap->Lock(&wicRect, WICBitmapLockFlags::WICBitmapLockRead, &pWicBitmapLock);
-				if (FAILED(hr))return false;
-
-				hr = pWicBitmapLock->GetStride(&pVideoFrame->uiStride);
-				if (FAILED(hr))return false;
-
-				pVideoFrame->nPixelSize = static_cast<size_t>(pVideoFrame->uiStride * pVideoFrame->iHeight);
-				pVideoFrame->pPixels = static_cast<unsigned char*>(malloc(pVideoFrame->nPixelSize));
-				if (pVideoFrame->pPixels == nullptr)return false;
-
-				hr = m_pWicBitmap->CopyPixels(nullptr, pVideoFrame->uiStride, static_cast<UINT>(pVideoFrame->nPixelSize), pVideoFrame->pPixels);
-				if (FAILED(hr))
-				{
-					free(pVideoFrame->pPixels);
-					return false;
-				}
-
-				return SUCCEEDED(hr);
-			}
+			*currentFrameTime = GetCurrentTimeInMilliSeconds();
 		}
+
+		unsigned int uiWidth = 0;
+		unsigned int uiHeight = 0;
+		HRESULT hr = m_pWicBitmap->GetSize(&uiWidth, &uiHeight);
+		if (FAILED(hr))return false;
+
+		pVideoFrame->iWidth = uiWidth;
+		pVideoFrame->iHeight = uiHeight;
+
+		CComPtr<IWICBitmapLock> pWicBitmapLock;
+		WICRect wicRect{ 0, 0, pVideoFrame->iWidth, pVideoFrame->iHeight };
+		hr = m_pWicBitmap->Lock(&wicRect, WICBitmapLockFlags::WICBitmapLockRead, &pWicBitmapLock);
+		if (FAILED(hr))return false;
+
+		hr = pWicBitmapLock->GetStride(&pVideoFrame->uiStride);
+		if (FAILED(hr))return false;
+
+		pVideoFrame->nPixelSize = static_cast<size_t>(pVideoFrame->uiStride * pVideoFrame->iHeight);
+		pVideoFrame->pPixels = static_cast<unsigned char*>(malloc(pVideoFrame->nPixelSize));
+		if (pVideoFrame->pPixels == nullptr)return false;
+
+		hr = m_pWicBitmap->CopyPixels(nullptr, pVideoFrame->uiStride, static_cast<UINT>(pVideoFrame->nPixelSize), pVideoFrame->pPixels);
+		if (FAILED(hr))
+		{
+			free(pVideoFrame->pPixels);
+			return false;
+		}
+
+		return SUCCEEDED(hr);
+	}
+
+	return false;
+}
+
+bool CMfVideoTransferor::TransferVideoFrame(ID2D1DeviceContext* const pD2d1DeviceContext, ID2D1Bitmap** pD2d1Bitmap, long long* currentFrameTime)
+{
+	if (pD2d1DeviceContext == nullptr)return false;
+
+	if (TransferVideoFrameToWicBitmap())
+	{
+		if (currentFrameTime != nullptr)
+		{
+			*currentFrameTime = GetCurrentTimeInMilliSeconds();
+		}
+
+		HRESULT hr = pD2d1DeviceContext->CreateBitmapFromWicBitmap(m_pWicBitmap, D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE)), pD2d1Bitmap);
+
+		return SUCCEEDED(hr);
 	}
 
 	return false;
@@ -139,5 +136,37 @@ bool CMfVideoTransferor::CheckWicBitmapSize(unsigned long uiWidth, unsigned long
 			return true;
 		}
 	}
+	return false;
+}
+
+bool CMfVideoTransferor::TransferVideoFrameToWicBitmap()
+{
+	if (m_pMfEngineEx != nullptr)
+	{
+		BOOL iRet = m_pMfEngineEx->HasVideo();
+		if (iRet)
+		{
+			long long llReadyFrame = 0;
+			HRESULT hr = m_pMfEngineEx->OnVideoStreamTick(&llReadyFrame);
+			if (SUCCEEDED(hr) && llReadyFrame >= 0)
+			{
+				unsigned long ulDestWidth = 0;
+				unsigned long ulDestHeight = 0;
+				bool bRet = GetVideoSize(&ulDestWidth, &ulDestHeight);
+				if (!bRet)return false;
+
+				bRet = CheckWicBitmapSize(ulDestWidth, ulDestHeight);
+				if (!bRet)return false;
+
+				RECT dstRect{ 0, 0, static_cast<LONG>(ulDestWidth), static_cast<LONG>(ulDestHeight) };
+				MFARGB bg{ 0, 0, 0, 0 };
+				MFVideoNormalizedRect normalisedRect{};
+				hr = m_pMfEngineEx->TransferVideoFrame(m_pWicBitmap, &normalisedRect, &dstRect, &bg);
+
+				return SUCCEEDED(hr);
+			}
+		}
+	}
+
 	return false;
 }
--- AyarabuPlayer/mf_video_transferor.h
+++ AyarabuPlayer/mf_video_transferor.h
@@ -1,4 +1,4 @@
-#ifndef MF_VIDEO_TRANSFEROR_H_
+﻿#ifndef MF_VIDEO_TRANSFEROR_H_
 #define MF_VIDEO_TRANSFEROR_H_
 
 /*
@@ -9,9 +9,12 @@
 * it is impossible to draw text on video frame.
 * Thus, frame-server mode is used to controll drawring order.
 * 
+* Transfer to DXGI surface seems not to be useful because it is basically the same as rendering mode.
+* 
 */
 
 #include <wincodec.h>
+#include <d2d1_1.h>
 
 #include "mf_media_player.h"
 
@@ -26,20 +29,24 @@ public:
 		int iWidth = 0;
 		int iHeight = 0;
 		unsigned int uiStride = 0;
-		unsigned char* pPixels = nullptr;
+		unsigned char* pPixels = nullptr; /* should be freed on user's side. */
 		size_t nPixelSize = 0;
-		long long llCurrentTime = 0;
 	};
-	bool TransferVideoFrame(SVideoFrame* pVideoFrame);
+	/// @brief Transfer as CPU image
+	bool TransferVideoFrame(SVideoFrame* pVideoFrame, long long *currentFrameTime);
+	/// @brief Transfer as GPU resource
+	bool TransferVideoFrame(ID2D1DeviceContext* const pD2d1DeviceContext, ID2D1Bitmap** pD2d1Bitmap, long long *currentFrameTime);
 
-	bool SetPlaybackWindow(HWND hWnd, UINT uMsg);
-	bool ResizeBuffer();
+	bool SetPlaybackWindow(HWND hWnd, UINT uMsg) override;
+	bool ResizeBuffer() override;
 private:
 	IWICBitmap *m_pWicBitmap = nullptr;
 
 	void ReleaseWicBitmap();
 	bool CreateWicBitmap(unsigned long uiWidth, unsigned long uiHeight);
 	bool CheckWicBitmapSize(unsigned long uiWidth, unsigned long uiHeight);
+
+	bool TransferVideoFrameToWicBitmap();
 };
 
 #endif // !MF_VIDEO_TRANSFEROR_H_
